f5bde38b5472f261ada3356468b8c47073410912
Justin Fofana <indigolasagna@gmail.com>
2018-08-23 16:00:47 -0400

    Salutation dans le log.

--- app/src/main/java/ca/cours5b5/justinfofana/Parametres.java
+++ app/src/main/java/ca/cours5b5/justinfofana/Parametres.java
@@ -2,12 +2,14 @@ package ca.cours5b5.justinfofana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Parametres extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MonEtiquette", "Bonjour!");
         setContentView(R.layout.activity_parametres);
     }
 }
